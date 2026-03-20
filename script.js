01a52f8e99d1940d1d2fa1f05b0d800a4f58c52b
Kanishk Jakhmola <kanishkjakhmola@gmail.com>
2026-03-20 07:30:18 +0530

    Refactor menu and scroll event handling

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ window.onscroll = () => {
     if (top >= offset && top < offset + height) {
       navLinks.forEach(links => links.classList.remove('active'));
       document.querySelector(`header nav a[href="#${id}"]`).classList.add('active');
-    
+
     sec.classList.add('show-animate');
     }
     else {
@@ -45,4 +45,3 @@ submitButton.addEventListener('click', () => {
 
   window.location.href = mailtoLink;
 });
-
